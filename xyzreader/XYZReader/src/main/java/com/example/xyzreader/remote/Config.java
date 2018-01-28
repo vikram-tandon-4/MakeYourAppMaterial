670a26a8f3f136f18a57063458a1e691d8ce9938
vikram-tandon-4 <vikramtandon4192@gmail.com>
2018-01-28 16:36:20 -0600

    Inital Commit

---
+++ xyzreader/XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -0,0 +1,21 @@
+package com.example.xyzreader.remote;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Config {
+    public static final URL BASE_URL;
+
+    static {
+        URL url = null;
+        try {
+//            url = new URL("https://dl.dropboxusercontent.com/u/231329/xyzreader_data/data.json" );
+//            url = new URL("https://raw.githubusercontent.com/Protino/dump/master/Lego/data.json" );
+            url = new URL("https://raw.githubusercontent.com/TNTest/xyzreader/master/data.json" );
+        } catch (MalformedURLException ignored) {
+            // TODO: throw a real error
+        }
+
+        BASE_URL = url;
+    }
+}
